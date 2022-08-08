83e3f909c90caa44817c639721ad6d943035be66
Talgat <tairovtalgat@gmail.com>
2022-08-08 19:24:59 +0600

    configured: svg as react components, global styles, absolute imports

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,11 @@
 import React, {FC, useState} from "react";
 import "./null.scss";
-import './App.css'
 import "./styles/global.scss";
 import {Routes, Route, BrowserRouter} from "react-router-dom";
-import {Coupon} from "../pages/Coupon";
+import {Coupon} from "@/pages/Coupon";
 import {Header} from "./Header";
 import {Footer} from "./Footer";
+import css from "./App.module.scss";
 
 
 export const App: FC = () => {
@@ -25,7 +25,7 @@ export const App: FC = () => {
 
 const LOL: FC = () => {
     return (
-        <div>
+        <div className={css.text}>
             Lol KEK
         </div>
     )
