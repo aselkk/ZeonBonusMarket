a201a0f2a4187bcb988718d9eed14fa9380992d0
Bolotbek <bolotbek.muratov@gmail.com>
2022-08-09 16:41:22 +0600

    finish design of footer, need links

--- src/app/Footer/index.tsx
+++ src/app/Footer/index.tsx
@@ -1,90 +1,105 @@
 import React, { FC } from "react";
+import { Link } from "react-router-dom";
 import css from "./styles.module.scss";
 import cn from "classnames";
-import { ReactComponent as FaceBookIcon } from "../../assets/icons/FB.svg";
-import { ReactComponent as IstagrammIcon } from "../../../src/assets/icons/INSTA.svg";
-import { ReactComponent as VKontakte } from "../../assets/icons/VK.svg";
-import { ReactComponent as Odnoklassniki } from "../../assets/icons/OKK.svg";
-import { ReactComponent as Mail } from "../../assets/icons/MAIL.svg";
-import { ReactComponent as Phone } from "../../assets/icons/TEL.svg";
-import { Link } from "react-router-dom";
+import { ReactComponent as FaceBookIcon } from "@/assets/icons/facebook.svg";
+import { ReactComponent as IstagrammIcon } from "@/assets/icons/instagramm.svg";
+import { ReactComponent as VKontakte } from "@/assets/icons/vkontakte.svg";
+import { ReactComponent as Odnoklassniki } from "@/assets/icons/odnoklassniki.svg";
+import { ReactComponent as Mail } from "@/assets/icons/mail.svg";
+import { ReactComponent as Phone } from "@/assets/icons/phone.svg";
 
 export const Footer: FC = () => {
   return (
     <div className={css.footer}>
       <div className={cn("container", css.container)}>
         <div className={css.box}>
-          <h3>Покупателям</h3>
+          <div className={css.title}>
+            <h3>Покупателям</h3>
+          </div>
           <ul>
             <li>
               <Link className={css.linkTo} to={"#"}>
+                {/* TODO: ссылки на страницы */}
                 <p>Как сделать заказ</p>
               </Link>
             </li>
             <li>
               <Link className={css.linkTo} to={"#"}>
+                {/* TODO: ссылки на страницы */}
                 <p>Способы оплаты</p>
               </Link>
             </li>
             <li>
               <Link className={css.linkTo} to={"#"}>
+                {/* TODO: ссылки на страницы */}
                 <p>Вопросы и ответы</p>
               </Link>
             </li>
             <li>
               <Link className={css.linkTo} to={"#"}>
+                {/* TODO: ссылки на страницы */}
                 <p>Политика конфиденциальности</p>
               </Link>
             </li>
           </ul>
         </div>
         <div className={css.box}>
-          <h3>Компания</h3>
+          <div className={css.title}>
+            <h3>Компания</h3>
+          </div>
           <ul>
             <li>
               <Link className={css.linkTo} to={"#"}>
+                {/* TODO: ссылки на страницы */}
                 <p>О нас</p>
               </Link>
             </li>
             <li>
               <Link className={css.linkTo} to={"#"}>
-                {" "}
+                {/* TODO: ссылки на страницы */}
+                <p>Контакты</p>
               </Link>
-              <p>Контакты</p>
             </li>
           </ul>
         </div>
         <div className={css.box}>
-          <h3>Мы в соц сетях</h3>
+          <div className={css.title}>
+            <h3>Мы в соц сетях</h3>
+          </div>
           <ul>
             <li>
               <Link className={css.linkTo} to={"#"}>
+                {/* TODO: ссылки на страницы */}
                 <div>
-                  <VKontakte className={css.testIcon} />
+                  <VKontakte className={css.icons} />
                 </div>
                 <p>Вконтакте</p>
               </Link>
             </li>
             <li>
               <Link className={css.linkTo} to={"#"}>
+                {/* TODO: ссылки на страницы */}
                 <div>
-                  <FaceBookIcon className={css.testIcon} />
+                  <FaceBookIcon className={css.icons} />
                 </div>
                 <p>Facebook</p>
               </Link>
             </li>
             <li>
               <Link className={css.linkTo} to={"#"}>
+                {/* TODO: ссылки на страницы */}
                 <div>
-                  <Odnoklassniki className={css.testIcon} />
+                  <Odnoklassniki className={css.icons} />
                 </div>
                 <p>Одноклассники</p>
               </Link>
             </li>
             <li>
               <Link className={css.linkTo} to={"#"}>
+                {/* TODO: ссылки на страницы */}
                 <div>
-                  <IstagrammIcon className={css.testIcon} />
+                  <IstagrammIcon className={css.icons} />
                 </div>
                 <p>Instagram</p>
               </Link>
@@ -92,20 +107,24 @@ export const Footer: FC = () => {
           </ul>
         </div>
         <div className={css.box}>
-          <h3>Свяжитесь с нами</h3>
+          <div className={css.title}>
+            <h3>Свяжитесь с нами</h3>
+          </div>
           <ul>
             <li>
               <Link className={css.linkTo} to={"#"}>
+                {/* TODO: ссылки на страницы */}
                 <div>
-                  <Mail className={css.testIcon} />
+                  <Mail className={css.icons} />
                 </div>
                 <p>mail@mix.kg</p>
               </Link>
             </li>
             <li>
               <Link className={css.linkTo} to={"#"}>
+                {/* TODO: ссылки на страницы */}
                 <div>
-                  <Phone className={css.testIcon} />
+                  <Phone className={css.icons} />
                 </div>
                 <p>+996 555 55 55 55</p>
               </Link>
