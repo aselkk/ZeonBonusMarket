7ecbfc264d7f37cb9056891b77809f93f5527a00
Talgat Tairov <38838673+slice312@users.noreply.github.com>
2022-08-14 19:53:11 +0600

    Feature/header 2x61yzc (#18)
    * remove categories
    
    * remove categories styles
    
    * add header menu burger
    
    * gg
    
    * cheburek
    
    Co-authored-by: “Asel” <aselkurmanbekk@gmail.com>

--- src/app/Header/index.tsx
+++ src/app/Header/index.tsx
@@ -1,29 +1,30 @@
-import React, {useLayoutEffect, useState, FC} from "react";
+import React, { useLayoutEffect, useState, FC } from "react";
+
 import {Mobile} from "./Mobile";
 import {Desktop} from "./Desktop";
 
 export const Header: FC = () => {
 
-    function useWindowSize() {
-        const [size, setSize] = useState([0, 0]);
-        useLayoutEffect(() => {
-            function updateSize() {
-                setSize([window.innerWidth, window.innerHeight]);
-            }
+  function useWindowSize() {
+    const [size, setSize] = useState([0, 0]);
+    useLayoutEffect(() => {
+      function updateSize() {
+        setSize([window.innerWidth, window.innerHeight]);
+      }
+      window.addEventListener('resize', updateSize);
+      updateSize();
+      return () => window.removeEventListener('resize', updateSize);
+    }, []);
+    return size;
+  }
+  
+  const [width, height] = useWindowSize();
+  console.log(width, height);
 
-            window.addEventListener('resize', updateSize);
-            updateSize();
-            return () => window.removeEventListener('resize', updateSize);
-        }, []);
-        return size;
-    }
-
-    const [width, height] = useWindowSize();
-
-    if (width < 768) {
-        return <Mobile/>;
-    } else {
-        return <Desktop/>;
-    }
+  if (width < 768) {
+    return <Mobile/>; 
+  } else {
+  return <Desktop/>;
+  }
 
 };
