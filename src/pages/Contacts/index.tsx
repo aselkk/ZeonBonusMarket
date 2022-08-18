7d3789ee9ed78e8468d629eff3dd863c6b760496
Talgat <tairovtalgat@gmail.com>
2022-08-18 14:49:56 +0600

    fix map on Contact

--- src/pages/Contacts/index.tsx
+++ src/pages/Contacts/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import cn from "classnames";
 
-import { MenuAboutUs } from "@/features/MenuMini";
+
+import {axiosInstance} from "@/shared/api";
+import {MenuAboutUs} from "@/features/MenuMini";
 import css from "./styles.module.scss";
 
 import {ReactComponent as PhoneIcon} from "@/assets/icons/phone.svg";
@@ -13,170 +15,180 @@ import {ReactComponent as VKontakteIcon} from "@/assets/icons/vkontakte.svg";
 import {ReactComponent as OdnoklassnikiIcon} from "@/assets/icons/odnoklassniki.svg";
 import {SimpleMap} from "@/shared/ui/GoogleMap";
 
-import map from "@/assets/images/mapImage.png"; // TODO: Асель добавит карту
-import { useSelector } from "react-redux";
+
+
 
 export const Contacts = () => {
-  const networkInfo = useSelector((state: any) => state.networkInfo.items);
+    const [contacts, setContacts] = React.useState<any>({});
+    // TODO: вернуть useSelector
 
-  return (
-    <div className={css.contacts}>
-      <div className={cn("container", css.container)}>
-        <MenuAboutUs />
-        <div className={css.content}>
-          <h3 className={css.title}>Контакты</h3>
-          <div className={css.text}>
-            <p>{networkInfo.description}</p>
-          </div>
-          <div className={css.box}>
-            <div className={css.innerBox}>
-              <h4>Наши телефоны:</h4>
-              <ul>
-                <li>
-                  <a rel="stylesheet" href={`tel:${networkInfo.phone1}`}>
-                    <div>
-                      <PhoneIcon className={css.icons} />
-                    </div>
-                    <p>{networkInfo.phone1}</p>
-                  </a>
-                </li>
-                <li>
-                  <a rel="stylesheet" href={`tel:${networkInfo.phone2}`}>
-                    <div>
-                      <PhoneIcon className={css.icons} />
-                    </div>
-                    <p>{networkInfo.phone2}</p>
-                  </a>
-                </li>
-                <li>
-                  <a rel="stylesheet" href={`tel:${networkInfo.phone3}`}>
-                    <div>
-                      <PhoneIcon className={css.icons} />
-                    </div>
-                    <p>{networkInfo.phone3}</p>
-                  </a>
-                </li>
-              </ul>
-              <div className={(css.innerBox, css.inMobile)}>
-                <h4>Email:</h4>
-                <ul>
-                  <li>
-                    <a rel="stylesheet" href={`mailto:${networkInfo.email}`}>
-                      <div>
-                        <MailIcon className={css.icons} />
-                      </div>
-                      <p>{networkInfo.email}</p>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
+    // TODO: have to move other file
+    const getContactsfromServer = () => {
+        return axiosInstance.get("/info/networks/");
+    };
 
-            <div className={css.line1}></div>
+    const getContactsfromServerAPI = async () => {
+        const response = await getContactsfromServer();
+        setContacts(response.data);
+    };
 
-            <div className={css.noMobile}>
-              <div className={css.innerBox}>
-                <div>
-                  <h5>Email:</h5>
-                  <ul>
-                    <li>
-                      <a rel="stylesheet" href={`mailto:${networkInfo.email}`}>
-                        <div>
-                          <MailIcon className={css.icons} />
+    React.useEffect(() => {
+        getContactsfromServerAPI();
+    }, [setContacts]);
+
+
+    return (
+        <div className={css.contacts}>
+            <div className={cn("container", css.container)}>
+                <MenuAboutUs />
+                <div className={css.content}>
+                    <h3 className={css.title}>Контакты</h3>
+                    <div className={css.text}>
+                        <p>{contacts.description}</p>
+                    </div>
+                    <div className={css.box}>
+                        <div className={css.innerBox}>
+                            <h4>Наши телефоны:</h4>
+                            <ul>
+                                <li>
+                                    <a rel="stylesheet" href={`tel:${contacts.phone1}`}>
+                                        <div>
+                                            <PhoneIcon className={css.icons} />
+                                        </div>
+                                        <p>{contacts.phone1}</p>
+
+                                    </a>
+
+                                </li>
+                                <li>
+                                    <a rel="stylesheet" href={`tel:${contacts.phone2}`}>
+
+                                        <div>
+                                            <PhoneIcon className={css.icons} />
+                                        </div>
+                                        <p>{contacts.phone2}</p>
+
+                                    </a>
+
+                                </li>
+                                <li>
+                                    <a rel="stylesheet" href={`tel:${contacts.phone3}`}>
+                                        <div>
+                                            <PhoneIcon className={css.icons} />
+                                        </div>
+                                        <p>{contacts.phone3}</p>
+
+                                    </a>
+
+                                </li>
+                            </ul>
+                            <div className={(css.innerBox, css.inMobile)}>
+                                <h4>Email:</h4>
+                                <ul>
+                                    <li>
+                                        <a rel="stylesheet" href={`mailto:${contacts.email}`}>
+                                            <div>
+                                                <MailIcon className={css.icons} />
+                                            </div>
+                                            <p>{contacts.email}</p>
+
+                                        </a>
+
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                        <p>{networkInfo.email}</p>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-                <div>
-                  <h5>Наш адрес:</h5>
-                  <ul>
-                    <li>
-                      <div>
-                        <LocationIcon className={css.icons} />
-                      </div>
-                      <p>{networkInfo.address}</p>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
 
-            <div className={css.line}></div>
+                        <div className={css.line1}></div>
 
-            <div className={css.innerBox}>
-              <h4>Мы в социальных сетях:</h4>
-              <ul>
-                <li>
-                  <a
-                    rel="stylesheet"
-                    href={networkInfo.facebook}
-                    target="_blank"
-                  >
-                    <div>
-                      <FaceBookIcon className={css.icons} />
+                        <div className={css.noMobile}>
+                            <div className={css.innerBox}>
+                                <div>
+                                    <h5>Email:</h5>
+                                    <ul>
+                                        <li>
+                                            <a rel="stylesheet" href={`mailto:${contacts.email}`}>
+                                                <div>
+                                                    <MailIcon className={css.icons} />
+                                                </div>
+                                                <p>{contacts.email}</p>
+
+
+                                            </a>
+
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div>
+                                    <h5>Наш адрес:</h5>
+                                    <ul>
+                                        <li>
+                                            <div>
+                                                <LocationIcon className={css.icons} />
+                                            </div>
+                                            <p>{contacts.address}</p>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className={css.line}></div>
+
+                        <div className={css.innerBox}>
+                            <h4>Мы в социальных сетях:</h4>
+                            <ul>
+                                <li>
+                                    <a rel="stylesheet" href={contacts.facebook} target="_blank">
+                                        <div>
+                                            <FaceBookIcon className={css.icons} />
+                                        </div>
+                                        <p>Facebook</p>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a rel="stylesheet" href={contacts.vkontakte} target="_blank">
+                                        <div>
+                                            <VKontakteIcon className={css.icons} />
+                                        </div>
+                                        <p>VKontakte</p>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a rel="stylesheet" href={contacts.instagram} target="_blank">
+                                        <div>
+                                            <InstagramIcon className={css.icons} />
+                                        </div>
+                                        <p>Instagram</p>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a rel="stylesheet" href={contacts.odnoklassniki} target="_blank">
+                                        <div>
+                                            <OdnoklassnikiIcon className={css.icons} />
+                                        </div>
+                                        <p>Odnoklassniki</p>
+                                    </a>
+                                </li>
+                            </ul>
+                            <div className={cn(css.innerBox, css.inMobile)}>
+                                <h4>Наш адрес:</h4>
+                                <ul>
+                                    <li>
+                                        <div>
+                                            <LocationIcon className={css.icons} />
+                                        </div>
+                                        <p>{contacts.address}</p>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                     <div className={css.mapContainer}>
                         <SimpleMap info = {contacts}/>
-                    <p>Facebook</p>
-                  </a>
-                </li>
-                <li>
-                  <a
-                    rel="stylesheet"
-                    href={networkInfo.vkontakte}
-                    target="_blank"
-                  >
-                    <div>
-                      <VKontakteIcon className={css.icons} />
                     </div>
-                    <p>VKontakte</p>
-                  </a>
-                </li>
-                <li>
-                  <a
-                    rel="stylesheet"
-                    href={networkInfo.instagram}
-                    target="_blank"
-                  >
-                    <div>
-                      <InstagramIcon className={css.icons} />
-                    </div>
-                    <p>Instagram</p>
-                  </a>
-                </li>
-                <li>
-                  <a
-                    rel="stylesheet"
-                    href={networkInfo.odnoklassniki}
-                    target="_blank"
-                  >
-                    <div>
-                      <OdnoklassnikiIcon className={css.icons} />
-                    </div>
-                    <p>Odnoklassniki</p>
-                  </a>
-                </li>
-              </ul>
-              <div className={(css.innerBox, css.inMobile)}>
-                <h4>Наш адрес:</h4>
-                <ul>
-                  <li>
-                    <div>
-                      <LocationIcon className={css.icons} />
-                    </div>
-                    <p>{networkInfo.address}</p>
-                  </li>
-                </ul>
-              </div>
+                </div>
             </div>
-          </div>
-          <div className={css.imageBox}>
-            <img className={css.image} src={map} alt="map" />
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
