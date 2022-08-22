04723abdeab9598c256e5312cbab5361a1e153f9
“Asel” <aselkurmanbekk@gmail.com>
2022-08-22 12:26:31 +0600

    add sort dropdown

--- src/pages/SearchResult/index.tsx
+++ src/pages/SearchResult/index.tsx
@@ -29,6 +29,8 @@ export const SearchResult = () => {
     
     const [sortOrder, setSortOrder] = useState(SortType.AlphabetAsc);
 
+    const [isOpened, setIsOpened] = useState(false)
+
     useEffect(() => {
         const resultCoupons =
             state.result.map((x: DTO.Coupon) => couponModel.convertToCouponInfo(x));
@@ -47,14 +49,32 @@ export const SearchResult = () => {
             <div className={cn("container", css.container)}>
                 <div className={css.header}>
                     <h2 className={css.title}>Результаты поиска</h2>
-                    <select name="" id="" value={sortOrder} onChange={(e: any) => {
+                    {/* <select name="" id="" value={sortOrder} onChange={(e: any) => {
                         setSortOrder(Number(e.target.value));
                     }}>
                         <option value={SortType.AlphabetAsc}>По алфавиту</option>
                         <option value={SortType.PriceAsc}>{"По цене (низкая > высокая)"}</option>
                         <option value={SortType.PriceDesc}>{"По цене (высокая > низкая) "}</option>
-                    </select>
-                    
+                    </select> */}
+                    <div className={css.sort}>
+                        <p onClick={() => setIsOpened(true)}>Сортировать по: ▾</p>
+                        <div style = {isOpened ? {display: 'flex'} : {display: 'none'}} className={css.sortList}>
+                            <h3 onClick={() => {
+                                setSortOrder(SortType.AlphabetAsc)
+                                setIsOpened(false);
+                            }}>Сортировать по: ▾</h3>
+                            <div onClick={() => {
+                                setSortOrder(SortType.PriceAsc)
+                                setIsOpened(false);
+                            }}>алфавиту</div>
+                            <div>{'По цене (низкая > высокая)'}</div>
+                            <div onClick={() => {
+                                setSortOrder(SortType.PriceDesc)
+                                setIsOpened(false);
+                            }}>{'По цене (высокая > низкая)'}</div>
+                        </div>
+                    </div>
+
                 </div>
                 <CardsContainer
                     cards={coupons}
